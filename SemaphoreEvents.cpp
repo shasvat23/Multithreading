616e92bff7d4634144a5811c0ba3fc796c767ce4
zee <shasvat23@gmail.com>
2020-08-17 20:23:52 -0400

    Zee resolved conflict

--- SemaphoreEvents.cpp
+++ SemaphoreEvents.cpp
@@ -18,15 +18,9 @@
 #include <string.h>
 
 
-<<<<<<< Updated upstream
+
 //this is just a test entry for git, SCP trying to create conflict
-=======
-<<<<<<< HEAD
-//this is just a test entry for git, trial 2
-=======
-//this is just a test entry for git, SCP trying to create conflict
->>>>>>> b110adc243da65cabad726f705dbd470c1a0e128
->>>>>>> Stashed changes
+//SCP was succesful in solving conflict
 
 void strNcpy(char *dst, const char *src, int max)
 	{
